fbe589c5d78a557ca5836cd37042d04bd6320f65
Dan G <ddgroleau@gmail.com>
2021-02-16 15:25:51 -0700

    complete architecture

---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Result = ({score, playAgain}) => (
+    <div className="score-board">
+        <div className="score">You scored {score} / 5 correct answers!</div>
+        <button className="playBtn" onClick={playAgain}>
+            Play again!
+        </button>
+    </div>
+)
+
+export default Result;
