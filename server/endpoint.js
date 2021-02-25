c04c94ca8a81549b28df55819acbd58926ba9b80
Dan G <ddgroleau@gmail.com>
2021-02-25 15:41:54 -0700

    update db

--- server/endpoint.js
+++ server/endpoint.js
@@ -7,7 +7,6 @@ const client = require('./database')
 Router.get('/endpoint', (request, response) => {
    const data = client.query('SELECT * FROM questions;', (err, results) => {
         if (err) console.log(err);
-        console.log(results.rows);
         response.send(results.rows);
     })
 });
