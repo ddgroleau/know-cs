b0dad29fc75207dd535516cb208b393e799c1f91
Dan G <ddgroleau@gmail.com>
2021-02-25 17:50:11 -0700

    update path

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', endpoint);
 
 app.get('/', (req,res)=> {
-    res.sendFile(path.resolve(_dirname, "/client/build.index.html"));
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
 });
 
 
